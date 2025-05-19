1e687c24cd01ac0b2403aec319bd11b4d6d83396
ThanhXT2002 <tranxuanthanhtxt2002@gmail.com>
2025-05-19 23:56:57 +0700

    add environments in gitignore

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://api-file-manager.tranxuanthanh.vn/api',
-};
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:7126/api', // Replace with your API URL
-};
-
-
